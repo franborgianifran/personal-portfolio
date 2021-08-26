e739ea836fef0a92066a6dfff2a5034d5e1e8ca6
FrancoBorgiani <franborgiani2003@gmail.com>
2021-08-26 20:36:02 -0300

    Ready to deploy

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
@@ -12,3 +13,15 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass('active');
     })
 });
+document.querySelector('.menu').addEventListener('click', ()=>{
+    document.querySelector('.menu').classList.remove('active')
+    document.querySelector('.menu-btn i').classList.remove('active');
+})
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY > 200){
+        document.querySelector('.scroll-up-btn').classList.add('show');
+    }
+    else{
+        document.querySelector('.scroll-up-btn').classList.remove('show');
+    }
+})
